3fc078bb85f630df2926ec106167c88cd40b2f93
avega <avega@819ff99d-f73b-0410-b8ac-dab01f8602e2>
2009-10-20 17:19:48 +0000

    Replaced license header: to Affero-GPL
    git-svn-id: https://svn.forge.morfeo-project.org/tidorbc/branches/CodeCleaning@1118 819ff99d-f73b-0410-b8ac-dab01f8602e2

--- source/TIDorbC/orb/include/TIDorb/core/util/Octet.h
+++ source/TIDorbC/orb/include/TIDorb/core/util/Octet.h
@@ -1,3 +1,41 @@
+/*//////////////////////////////////////////////////////////////////////////////////
+//
+// File:        Octet.h
+// Description: 
+//
+// Rel:         01.00
+// Created:     
+// Author:      
+//
+// Revised:
+//
+// (C) Copyright 2009 Telefonica Investigacion y Desarrollo
+//     S.A.Unipersonal (Telefonica I+D)
+//
+// This file is part of Morfeo CORBA Platform.
+//
+// Morfeo CORBA Platform is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Morfeo CORBA Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Morfeo CORBA Platform. If not, see
+//
+//   http://www.gnu.org/licenses
+//
+// Info about members and contributors of the MORFEO project
+// is available at
+//
+//   http://morfeo-project.org
+//
+///////////////////////////////////////////////////////////////////////////////////*/
+
 #ifndef __TIDorb_core_util_Octet_H_
 #define __TIDorb_core_util_Octet_H_
 
--- source/TIDorbC/orb/include/TIDorb/core/util/exceptions/REBIND_Creator.h
+++ source/TIDorbC/orb/include/TIDorb/core/util/exceptions/REBIND_Creator.h
@@ -1,3 +1,41 @@
+/*//////////////////////////////////////////////////////////////////////////////////
+//
+// File:        
+// Description: 
+//
+// Rel:         01.00
+// Created:     
+// Author:      
+//
+// Revised:
+//
+// (C) Copyright 2009 Telefonica Investigacion y Desarrollo
+//     S.A.Unipersonal (Telefonica I+D)
+//
+// This file is part of Morfeo CORBA Platform.
+//
+// Morfeo CORBA Platform is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Morfeo CORBA Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Morfeo CORBA Platform. If not, see
+//
+//   http://www.gnu.org/licenses
+//
+// Info about members and contributors of the MORFEO project
+// is available at
+//
+//   http://morfeo-project.org
+//
+///////////////////////////////////////////////////////////////////////////////////*/
+
 #include "TIDorb/core/util/exceptions.h"
 
 #ifndef _TIDORB_CORE_UTIL_EXCEPTIONS_REBIND_CREATOR_H_
