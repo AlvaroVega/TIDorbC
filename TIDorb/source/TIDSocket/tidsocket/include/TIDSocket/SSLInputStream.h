9a275fb85e64efc15982eb8042cd561491bc2efa
avega <avega@819ff99d-f73b-0410-b8ac-dab01f8602e2>
2011-03-29 06:17:47 +0000

    Affero-GPL header added to some files
    git-svn-id: https://svn.forge.morfeo-project.org/tidorbc/branches/CodeCleaning@1357 819ff99d-f73b-0410-b8ac-dab01f8602e2

--- TIDorb/source/TIDSocket/tidsocket/include/TIDSocket/SSLInputStream.h
+++ TIDorb/source/TIDSocket/tidsocket/include/TIDSocket/SSLInputStream.h
@@ -8,13 +8,30 @@
 //
 // Revised:
 //
-// Copyright 2009 Telefonica, I+D. Printed in Spain (Europe). All Rights
-// Reserved.
-// The copyright to the software program(s) is property of Telefonica I+D.
-// The program(s) may be used and or copied only with the express written
-// consent of Telefonica I+D or in acordance with the terms and conditions
-// stipulated in the agreement/contract under which the program(s) have
-// been supplied.
+// (C) Copyright 2009 Telefonica Investigacion y Desarrollo
+//     S.A.Unipersonal (Telefonica I+D)
+//
+// This file is part of Morfeo CORBA Platform.
+//
+// Morfeo CORBA Platform is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Morfeo CORBA Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Morfeo CORBA Platform. If not, see
+//
+//   http://www.gnu.org/licenses
+//
+// Info about members and contributors of the MORFEO project
+// is available at
+//
+//   http://morfeo-project.org
 //
 ///////////////////////////////////////////////////////////////////////////
 
